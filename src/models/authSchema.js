8cc1e4a74b06522f70d5b10136b1cac98f2cbfac
Weslen Mendes <weslenmends@gmail.com>
2022-05-05 15:55:48 -0300

    fix: changed the password validation regex and removed the password confirmation field from the schema

--- src/models/authSchema.js
+++ src/models/authSchema.js
@@ -1,5 +1,9 @@
 import Joi from "joi";
 
+const regexPassword = new RegExp(
+  "^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$"
+);
+
 const signInSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
@@ -8,8 +12,7 @@ const signInSchema = Joi.object({
 const signUpSchema = Joi.object({
   name: Joi.string().alphanum().min(3).max(30).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{8,30}$")).required(),
-  confirmPassword: Joi.ref("password"),
+  password: Joi.string().pattern(regexPassword).required(),
 });
 
 export { signInSchema, signUpSchema };
